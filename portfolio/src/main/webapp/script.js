9eee3362a5276c5c05fe511ab988fba3330adcf5
Parth Vora <parthbhayani1230@gmail.com>
2020-03-01 00:24:02 -0500

    Add world map and pie chart to home site
    Remove public API key by linking to ignored file

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,9 +12,17 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- * Adds a random greeting to the page.
- */
+var mykey = config.MY_KEY;
+google.charts.load("current", { packages: ["corechart"] });
+google.charts.setOnLoadCallback(drawChart);
+google.charts.load("current", {
+  packages: ["geochart"],
+  // Note: you will need to get a mapsApiKey for your project.
+  // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
+  mapsApiKey: mykey
+});
+google.charts.setOnLoadCallback(drawRegionsMap);
+
 function addRandomGreeting() {
   const greetings = [
     "I live in New Jersey",
@@ -75,4 +83,62 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
   captionText.innerHTML = dots[slideIndex - 1].alt;
+}
+
+/** Creates a chart and adds it to the page. */
+function drawChart() {
+  const data = new google.visualization.DataTable();
+  data.addColumn("string", "Activity");
+  data.addColumn("number", "Count");
+  data.addRows([
+    ["Sleep", 15],
+    ["Class", 20],
+    ["Work", 15],
+    ["Sit in Traffic", 20],
+    ["Drink Boba", 5],
+    ["Weekly Meetings", 10],
+    ["Walk my Dog", 15]
+  ]);
+
+  const options = {
+    title: "Weekly Activites",
+    width: 320,
+    height: 200
+  };
+
+  const chart = new google.visualization.PieChart(
+    document.getElementById("chart-container")
+  );
+  chart.draw(data, options);
+}
+
+function drawRegionsMap() {
+  var data = google.visualization.arrayToDataTable([
+    ["Country", "Order"],
+    ["United States", 0],
+    ["Greenland", 1],
+    ["England", 2],
+    ["Italy", 3],
+    ["France", 4],
+    ["Japan", 5],
+    ["Finland", 6],
+    ["Germany", 7],
+    ["China", 8],
+    ["Spain", 9],
+    ["Barcelona", 10],
+    ["Switzerland", 11],
+    ["Australia", 12]
+  ]);
+
+  var options = {
+    colorAxis: { colors: ["#7dbfdf", "#12127c"] },
+    backgroundColor: "#81d4fa",
+    datalessRegionColor: "#f8bbd0",
+    defaultColor: "#f5f5f5"
+  };
+
+  var chart = new google.visualization.GeoChart(
+    document.getElementById("geochart-colors")
+  );
+  chart.draw(data, options);
 }
